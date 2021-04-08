3716a42ac79d22b2d063c2b507333a15b6b58caf
Alisa-khodadadi <61903569+Alisa-khodadadi@users.noreply.github.com>
2021-04-08 22:46:44 +0430

    before pulling-database

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
-import './styles/components/App.scss';
+import "./styles/components/App.scss";
 
 function App() {
+  const onsubmit = (e) => {
+    e.preventDefault();
+    console.log("form submited");
+    const text = e.target.elements.text.value;
+    console.log(text);
+  };
+
   return (
     <div className="App">
       <h1>APP BOILERPLATE</h1>
+      <form onSubmit={onsubmit}>
+        <input type="text" name="text" />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
